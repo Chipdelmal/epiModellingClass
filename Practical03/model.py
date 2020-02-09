0eecc4b0216fc22799baabe92645271e89afbc80
chipdelmal <chipdelmal@gmail.com>
2020-02-09 12:35:49 -0800

    Several fixes to Gonorrhea

--- Practical03/model.py
+++ Practical03/model.py
@@ -6,3 +6,20 @@ def dGonorrhea(y, t, N, beta, partRate, rec):
     dSdt = -partRate*beta*I*S/N + rec*I
     dIdt = partRate*beta*I*S/N-rec*I
     return (dSdt, dIdt)
+
+
+def dGonorrheaHeterogeneous(y, t, N, beta, cl, cm, nh, nl, rec):
+    Sl, Sh, Il, Ih = y
+    # Auxiliary functions
+    ch = (cm - cl * nl) / (nh)
+    gh = (ch * nh) / (ch * nh + cl * nl)
+    gl = 1 - gh
+    pt = gh * Ih + gl * Il
+    ll = cl * beta * pt
+    lh = ch * beta * pt
+    # ODEs
+    dSldt = -ll * Sl + rec * Il
+    dShdt = -lh * Sh + rec * Ih
+    dIldt = ll * Sl - rec * Il
+    dIhdt = lh * Sh - rec * Ih
+    return (dSldt, dShdt, dIldt, dIhdt)
